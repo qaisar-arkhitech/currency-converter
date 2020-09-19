b8ba4a88955cc301699fdf762962be3b77e6d423
Qaisar Irfan <qaisar.irfan.2888@gmail.com>
2020-09-19 12:07:11 +0500

    setup eslint with prettier for vscode

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,28 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  extends: ["airbnb", "prettier", "prettier/react"],
+  rules: {
+    "no-named-as-default-member": "off",
+    "import/no-named-as-default": "off",
+    "react/forbid-prop-types": "off",
+    "react/jsx-filename-extension": [
+      1,
+      {
+        extensions: [".js", ".jsx"],
+      },
+    ],
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        singleQuote: false,
+        printWidth: 100,
+        semi: false,
+        tabWidth: 2,
+        useTabs: false,
+        bracketSpacing: false,
+        jsxBracketSameLine: true,
+      },
+    ],
+  },
+  plugins: ["prettier"],
+}
