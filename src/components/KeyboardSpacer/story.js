f1ab2c61378b3dc726b96cad610143d23986aa64
Qaisar Irfan <qaisar.irfan.2888@gmail.com>
2020-09-20 01:23:45 +0500

    added components Keyboard Spacer

---
+++ src/components/KeyboardSpacer/story.js
@@ -0,0 +1,13 @@
+import React from "react"
+import KeyboardSpacer from "."
+
+export default {
+  title: "KeyboardSpacer",
+  storyFn() {
+    return (
+      <div>
+        <KeyboardSpacer />
+      </div>
+    )
+  },
+}
