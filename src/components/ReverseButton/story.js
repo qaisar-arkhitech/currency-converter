9f86f89e96aa09008dfab338f616b10a122203fa
Qaisar Irfan <qaisar.irfan.2888@gmail.com>
2020-09-20 22:54:23 +0500

    ReverseButton added

---
+++ src/components/ReverseButton/story.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReverseButton from "."
+
+export default {
+  title: "ReverseButton",
+  storyFn() {
+    return (
+      <div>
+        <ReverseButton />
+      </div>
+    )
+  },
+}
