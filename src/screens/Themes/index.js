8ce3a3d565df2441f60b838c88abb73b46d4faf7
Qaisar Irfan <qaisar.irfan.2888@gmail.com>
2020-09-20 13:30:58 +0500

    RowSeparator added and update themes color to style

--- src/screens/Themes/index.js
+++ src/screens/Themes/index.js
@@ -1,45 +1,52 @@
 import React from "react"
-import {View, SafeAreaView, StatusBar, ScrollView} from "react-native"
+import {View, SafeAreaView, ScrollView, StatusBar} from "react-native"
 import PropTypes from "prop-types"
 import flow from "lodash/flow"
 import Entypo from "react-native-vector-icons/Entypo"
+import Color from "color"
 import themeStyles from "./styles"
 import connect from "./connect"
-import {RowItem} from "../../components/RowItem"
+import {RowItem, RowSeparator} from "../../components/RowItem"
 
 // Themes Component content
 export const Themes = ({themes, changeTheme, defaultTheme, styleableTheme}) => {
-  const colors = Object.keys(themes)
+  const colors = Object.keys(themes) || []
   const styles = themeStyles(styleableTheme)
   return (
     <SafeAreaView style={styles.root}>
-      <StatusBar barStyle="dark-content" />
+      <StatusBar barStyle="default" backgroundColor={styleableTheme[500]} />
       <ScrollView>
         {colors.map((color, index) => (
-          <RowItem
-            key={`theme-${index + 1}`}
-            title={`Theme ${index + 1}`}
-            onPress={() => changeTheme(color)}
-            rightIcon={
-              <View
-                style={{
-                  flexDirection: "row",
-                }}>
-                {defaultTheme === color ? (
-                  <View style={{marginRight: 12}}>
-                    <Entypo name="check" size={20} />
-                  </View>
-                ) : null}
+          <React.Fragment key={`theme-${index + 1}`}>
+            <RowItem
+              title={`Theme ${index + 1}`}
+              onPress={() => changeTheme(color)}
+              rightIcon={
                 <View
                   style={{
-                    width: 20,
-                    height: 20,
-                    backgroundColor: color,
-                  }}
-                />
-              </View>
-            }
-          />
+                    flexDirection: "row",
+                  }}>
+                  {defaultTheme === color ? (
+                    <View style={{marginRight: 12}}>
+                      <Entypo name="check" size={20} />
+                    </View>
+                  ) : null}
+                  <View
+                    style={{
+                      width: 20,
+                      height: 20,
+                      backgroundColor: color,
+                    }}
+                  />
+                </View>
+              }
+            />
+            <RowSeparator
+              style={{
+                backgroundColor: Color(styleableTheme[50]).darken(0.04).hex(),
+              }}
+            />
+          </React.Fragment>
         ))}
       </ScrollView>
     </SafeAreaView>
